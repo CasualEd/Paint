233067bf57d3d1a65829b6ef8329a2c234fb74be
CasualEd <94349386+CasualEd@users.noreply.github.com>
2021-11-19 22:17:53 +0100

    An undo button, some bug fixes and more comments

--- Paint/Paint/src/Drawing.java
+++ Paint/Paint/src/Drawing.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import static java.lang.Math.*;
 
 public class Drawing extends JPanel implements MouseMotionListener, MouseListener {
-    protected static ArrayList<Figure> List= new ArrayList<Figure>();
+    protected ArrayList<Figure> List= new ArrayList<Figure>();
     private Color c;
     private String nameFigure;
     protected int x;
@@ -21,7 +21,8 @@ public class Drawing extends JPanel implements MouseMotionListener, MouseListene
         this.c=Color.BLACK;
         this.nameFigure="Rectangle";
         this.setBackground(Color.WHITE);
-        this.addMouseListener(this);
+        this.addMouseListener(this); /*We add both listeners in order to know when the mouse is being pressed and when it is dragged*/
+        this.addMouseMotionListener(this);
 
     }
     public void setColor(Color color) {
@@ -32,14 +33,14 @@ public class Drawing extends JPanel implements MouseMotionListener, MouseListene
         return c;
     }
 
-    public static ArrayList<Figure> getList() {
-        return List;
+    public ArrayList<Figure> getList() {
+        return this.List;
     }
 
     public void setNameFigure(String nameFigure) {
         this.nameFigure = nameFigure;
     }
-    protected void paintComponent(Graphics g){ /*This function is used to redraw all the figures */
+    protected void paintComponent(Graphics g){ /*This function is used to redraw all the figures by clearing the graphic and then drawing all the figures in the list */
         super.paintComponent(g);
         if(List!=null){
             for(Figure figure : this.List){
@@ -57,42 +58,43 @@ public class Drawing extends JPanel implements MouseMotionListener, MouseListene
 
     @Override
     public void mousePressed(MouseEvent e) {  /*We create the figure when the user presses on the mouse */
-        /*System.out.println("Pressed");*/
         this.x=e.getX();
         this.y=e.getY();
-        if(this.nameFigure.equals("Circle")){
-            /*System.out.println("Daniel");*/
-            List.add(new Circle(x,y,c));
+        if(this.nameFigure.equals("Circle")){  /* We check for every different type of figure there is to create the right type of figure*/
+            this.List.add(new Circle(x,y,c));
         }
         else if(this.nameFigure.equals("Rectangle")){
-            /*System.out.println("Daniel");*/
-            List.add(new Rectangle(x,y,c));
+            this.List.add(new Rectangle(x,y,c));
         }
         else if(this.nameFigure.equals("Ellipse")){
-            /* System.out.println("Daniel");*/
-            List.add(new Ellipse(x,y,c));
+            this.List.add(new Ellipse(x,y,c));
         }
         else if(this.nameFigure.equals("Square")){
-            /*System.out.println("Daniel");*/
-            List.add(new Square(x,y,c));
+            this.List.add(new Square(x,y,c));
         }
+        else if(this.nameFigure.equals("UpArc")){
+            this.List.add(new UpArc(x,y,c));
+        }
+        else if(this.nameFigure.equals("DownArc")){
+            this.List.add(new DownArc(x,y,c));
+        }
+
 
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) { /*And draw it when the user releases the mouse*/
+    public void mouseReleased(MouseEvent e) { /*And finish the figure when the user releases the mouse*/
         /*System.out.println("Released");*/
         this.x=e.getX();
         /*System.out.println(x);*/
         this.y=e.getY();
         /*System.out.println(y);*/
-        Figure f= List.get(List.size()-1);
+        Figure f= this.List.get(List.size()-1); /*Since no other figure could have been created, the last figure is the one that has not be drawn yet */
         int px=f.getOrigin().getX();
         int py=f.getOrigin().getY();
         f.getOrigin().setX(min(this.x,px));
         f.getOrigin().setY(min(this.y,py));
         f.setBoundingBox(abs(this.y-py),abs(this.x-px));
-        f.draw(this.getGraphics());
         paintComponent(this.getGraphics());
     }
 
@@ -108,8 +110,23 @@ public class Drawing extends JPanel implements MouseMotionListener, MouseListene
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        this.x=e.getX();
 
+        this.y=e.getY();
+
+        Figure f= this.List.get(this.List.size()-1); /*This function was created so the user knows what his figure is going to look like */
+        int px=f.getOrigin().getX();
+        int py=f.getOrigin().getY();
+        f.getOrigin().setX(min(this.x,px));
+        f.getOrigin().setY(min(this.y,py));
+        f.setBoundingBox(abs(this.y-py),abs(this.x-px));
+        paintComponent(this.getGraphics());
+        f.getOrigin().setX(px); /*We reset the origin of the figure everytime the figure has been drawn so it does not lose it's origin point */
+        f.getOrigin().setY(py);
     }
+
+
+
 
     @Override
     public void mouseMoved(MouseEvent e) {
@@ -118,14 +135,13 @@ public class Drawing extends JPanel implements MouseMotionListener, MouseListene
     public void save(String filename){ /*We create the save function that allows to save drawings */
         try{
 
-            FileOutputStream fos = new FileOutputStream("C:\\Users\\dache\\Documents\\Drawings\\"+filename);
+            FileOutputStream fos = new FileOutputStream(filename);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeInt(List.size());
-            for(Figure f : List){
+            oos.writeInt(this.List.size());
+            for(Figure f : this.List){
                 oos.writeObject(f);
             }
             oos.close();
-            File file=new File("C:\\Users\\dache\\Documents\\Drawings\\"+filename);
 
         }
         catch (Exception e){
--- Paint/Paint/src/Window.java
+++ Paint/Paint/src/Window.java
@@ -19,6 +19,7 @@ public class Window extends JFrame implements ActionListener {
     JMenuItem N_Drawing=new JMenuItem("New");
     JMenuItem SaveD=new JMenuItem("Save");
     JMenuItem OpenD= new JMenuItem("Open");
+    JMenuItem Undo = new JMenuItem("Undo");
     JMenuItem Quit=new JMenuItem("Quit");
     public Window(String Title,int x,int y){
 
@@ -31,6 +32,7 @@ public class Window extends JFrame implements ActionListener {
         D.add(N_Drawing); /*Creating the menu */
         D.add(OpenD);
         D.add(SaveD);
+        D.add(Undo);
         P.add(Author);
         D.addSeparator();
         D.add(Quit);
@@ -41,18 +43,19 @@ public class Window extends JFrame implements ActionListener {
         BottomPanel.setLayout(new GridLayout(1,2));
         JPanel ColorPanel = new JPanel();
         JPanel FigurePanel = new JPanel();
-        ColorPanel.setLayout(new GridLayout(2,4));
-        FigurePanel.setLayout(new GridLayout(2,2));
+        ColorPanel.setLayout(new GridLayout(3,3)); /* We create the bottom panel with all the colors */
+        FigurePanel.setLayout(new GridLayout(3,2));/* We create the bottom panel with all the figures */
         JButton Red = new JButton("Red");
         Red.setBackground(Color.RED);
         JButton Blue = new JButton("Blue");
-        Blue.setForeground(Color.WHITE);
         Blue.setBackground(Color.BLUE);
         JButton Green = new JButton("Green");
         Green.setBackground(Color.GREEN);
         JButton Black = new JButton("Black");
-        Black.setForeground(Color.WHITE);
         Black.setBackground(Color.BLACK);
+        Black.setForeground(Color.WHITE);
+        JButton White= new JButton("White");
+        White.setForeground(Color.BLACK);
         JButton Orange = new JButton("Orange");
         Orange.setBackground(Color.ORANGE);
         JButton Yellow = new JButton("Yellow");
@@ -61,12 +64,18 @@ public class Window extends JFrame implements ActionListener {
         Pink.setBackground(Color.PINK);
         JButton Magenta = new JButton("Magenta");
         Magenta.setBackground(Color.MAGENTA);
-        JButton Ellipse = new JButton("Ellipse");
+        /*JButton Cyan = new JButton("Cyan");
+        Cyan.setBackground(Color.CYAN);*/
+        JButton Ellipse = new JButton("Ellipse"); /* We add the different figures*/
         JButton Square = new JButton("Square");
         JButton Rectangle = new JButton("Rectangle");
         JButton Circle =new JButton("Circle");
+        JButton UpArc = new JButton("UpArc");
+        JButton DownArc = new JButton("DownArc");
         ColorPanel.add(Red);
         ColorPanel.add(Blue);
+        /*ColorPanel.add(Cyan);*/
+        ColorPanel.add(White);
         ColorPanel.add(Black);
         ColorPanel.add(Green);
         ColorPanel.add(Orange);
@@ -77,14 +86,16 @@ public class Window extends JFrame implements ActionListener {
         FigurePanel.add(Square);
         FigurePanel.add(Rectangle);
         FigurePanel.add(Circle);
-        BottomPanel.add(ColorPanel,"East");
+        FigurePanel.add(UpArc);
+        FigurePanel.add(DownArc);
+        BottomPanel.add(ColorPanel,"East");/* We implement the color panel and the figure panel in the main bottom panel*/
         BottomPanel.add(FigurePanel,"West");
 
         contentPanel.add(BottomPanel,"South");
         contentPanel.add(draw,"Center");
         this.setVisible(true);
 
-        for(Component Button: ColorPanel.getComponents()){ /*On rend les boutons interactifs*/
+        for(Component Button: ColorPanel.getComponents()){ /*We make the buttons interactive*/
             if(Button instanceof JButton){
                 ((JButton) Button).addActionListener(this);}
             }
@@ -94,7 +105,7 @@ public class Window extends JFrame implements ActionListener {
                 ((JButton) Button).addActionListener(this);
             }
         }
-        for(Component Menu: br.getComponents()){
+        for(Component Menu: br.getComponents()){ /* We make the menu interactive*/
             for(Component Button  :((JMenu) Menu).getMenuComponents())
             if(Button instanceof JMenuItem){
                 ((JMenuItem) Button).addActionListener(this);
@@ -105,10 +116,10 @@ public class Window extends JFrame implements ActionListener {
 
 
     }
-    public void open(String filename){
+    public void open(String filename){ /*This function is used to open a previous drawing and recreate it*/
         int length=0;
         Figure f=null;
-        try (FileInputStream fis = new FileInputStream("C:\\Users\\dache\\Documents\\Drawings\\"+filename);
+        try (FileInputStream fis = new FileInputStream(filename);
              ObjectInputStream ois = new ObjectInputStream(fis)){
             length = ois.readInt();
             for(int i=0;i<length;i++){
@@ -124,7 +135,7 @@ public class Window extends JFrame implements ActionListener {
     }
     public void actionPerformed(ActionEvent e) {
         String cmd=e.getActionCommand();
-        switch(cmd){
+        switch(cmd){ /* We implement the actions for the different buttons here */
             case "Red":
                 draw.setColor(Color.RED);
                 break;
@@ -141,7 +152,6 @@ public class Window extends JFrame implements ActionListener {
                 draw.setColor(Color.BLACK);
                 break;
             case "Magenta":
-
                 draw.setColor(Color.MAGENTA);
                 break;
             case "Pink":
@@ -149,6 +159,12 @@ public class Window extends JFrame implements ActionListener {
                 break;
             case "Orange":
                 draw.setColor(Color.ORANGE);
+                break;
+            case "White":
+                draw.setColor(Color.WHITE);
+                break;
+            /*case "Cyan":
+                draw.setColor(Color.CYAN);*/
             case "Ellipse":
                 draw.setNameFigure("Ellipse");
                 break;
@@ -161,6 +177,12 @@ public class Window extends JFrame implements ActionListener {
             case "Circle":
                 draw.setNameFigure("Circle");
                 break;
+            case "DownArc":
+                draw.setNameFigure("DownArc");
+                break;
+            case "UpArc":
+                draw.setNameFigure("UpArc");
+                break;
             case "Save":
                 String filename = (String)JOptionPane.showInputDialog(this, "Input the name of the file",
                 "Save", JOptionPane.PLAIN_MESSAGE, null, null, "Drawing");
@@ -169,12 +191,16 @@ public class Window extends JFrame implements ActionListener {
                     draw.save(filename);
                 }
                 break;
+            case "Undo": /*Delete the previously drawn figure*/
+                draw.getList().remove(draw.getList().size()-1);
+                draw.paintComponent(draw.getGraphics());
+                break;
             case "Open":
                 String filename1 = (String)JOptionPane.showInputDialog(this, "Input the name of the file",
                         "Save", JOptionPane.PLAIN_MESSAGE, null, null, "Drawing");
-                System.out.println(filename1); /* To avoid having twice the same file name and thus causing an error, we ask the user for a filename */
+                System.out.println(filename1); /* In order for the user to choose his file, we ask the user for a filename */
                 if(filename1!=null) {
-                    draw.getList().clear();
+                    draw.getList().clear(); /* We reset the figure List on order to draw the opened drawing */
                     open(filename1);
                 }
                 break;
@@ -186,15 +212,14 @@ public class Window extends JFrame implements ActionListener {
                 info.showInternalMessageDialog(info,"Authors: Edwin Kergoat is currently a student at the ENSEA where he's completing a project on a program that would allow computer users to draw on their computer.","About us",JOptionPane.INFORMATION_MESSAGE);
                 break;
             case "New":/* To have a new file, we only need to erase all the figures in the figure list and ask to redraw all the figures in the list*/
-                draw.getList().clear(); /*But since there are none it creates a blank space*/
-                draw.paintComponent(draw.getGraphics());
+                Window win = new Window("Paint",800,600);
                 break;
 
         }
 
     }
     public static void main(){
-        Window win = new Window("Paint it black",800,600);
+        Window win = new Window("Paint",800,600);
 
     }
 }
